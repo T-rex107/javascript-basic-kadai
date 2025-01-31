d86da8aae67849af25a1500d0533b2ac222bb562
T-rex107 <makaronieri@gmail.com>
2025-01-31 21:59:11 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,4 +2,5 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-  text.textContent = "クリックしました";
+  text.textContent = "クリックしました"; 
+});
